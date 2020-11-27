3169ee01e7cde852b376057bbab0f2c1579952dc
iamraghudb <54179651+iamraghudb@users.noreply.github.com>
2020-11-27 10:00:25 +0530

    Updates

---
+++ precache-manifest.7330192ce84cb4b8164f0517fa23936c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eaac66e079d0cbe080537a405d06d25",
+    "url": "/tabs/index.html"
+  },
+  {
+    "revision": "6297af125da65f682fc7",
+    "url": "/tabs/static/css/main.77ad5ced.chunk.css"
+  },
+  {
+    "revision": "1fd6ce2224cf0d523c9a",
+    "url": "/tabs/static/js/2.bcafc819.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tabs/static/js/2.bcafc819.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6297af125da65f682fc7",
+    "url": "/tabs/static/js/main.48e32d74.chunk.js"
+  },
+  {
+    "revision": "c99201bb389f8c23b139",
+    "url": "/tabs/static/js/runtime-main.e9ac576b.js"
+  }
+]);
